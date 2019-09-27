ea36fdd553df00a906abdec3f9274fa5d8d76776
kahst <kahst@hrz.tu-chemnitz.de>
2019-09-27 13:16:07 +0200

    URL update

--- model/fetch_model.sh
+++ model/fetch_model.sh
@@ -1,1 +1,1 @@
-wget https://tuc.cloud/index.php/s/m9smX4FkqmJaxLW -O model/BirdNET_Soundscape_Model.pkl
+wget https://tuc.cloud/index.php/s/m9smX4FkqmJaxLW/download -O model/BirdNET_Soundscape_Model.pkl
