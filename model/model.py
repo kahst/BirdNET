49c1262bb9ce61af3e9e2d50c55f4a3c4cb96913
kahst <kahst@hrz.tu-chemnitz.de>
2019-10-07 20:49:04 +0200

    DS Block

--- model/model.py
+++ model/model.py
@@ -98,10 +98,10 @@ def resblock(net_in, filters, kernel_size, stride=1, preactivated=True, block_id
     # Dropout Layer
     net = l.DropoutLayer(net)        
 
-    # Second Convolution (make 1x1 if downsample block)
+    # Second Convolution
     net = l.batch_norm(l.Conv2DLayer(net,
                         num_filters=filters,
-                        filter_size=k_size,
+                        filter_size=kernel_size,
                         pad='same',
                         stride=1,
                         nonlinearity=None))
